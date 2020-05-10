ea196a6af53006267053e821fed3b314b343ecd2
JuStPiNkCoDeR <44189902+JuStPiNkCoDeR@users.noreply.github.com>
2020-05-10 22:47:04 +0300

    small layout works

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Row, Col} from 'react-flexbox-grid';
+import {Col} from 'react-flexbox-grid';
 import styled from 'styled-components';
-import {Container} from '../lib/Container';
+import {Container, Content} from '../lib/Container';
 import Logo from '../assets/images/logo.png';
 
-export const Header = () => {
-  return (
+export const Header = () => (
+  <>
     <Container>
       <HeaderRow middle="xs">
         <Col xs={3}>
@@ -19,10 +19,27 @@ export const Header = () => {
         </Col>
       </HeaderRow>
     </Container>
-  );
-};
+    <NavContainer>
+      <Content middle="xs">
+        <Col xs={8} xsOffset={2}>
+          <NavLinksBlock>
+            <p>constructor</p>
+            <p>help</p>
+            <p>users panels</p>
+            <p>faq</p>
+            <p>about</p>
+            <p>blog</p>
+          </NavLinksBlock>
+        </Col>
+        <Col xs={1} xsOffset={1}>
+          Icons
+        </Col>
+      </Content>
+    </NavContainer>
+  </>
+);
 
-const HeaderRow = styled(Row)`
+const HeaderRow = styled(Content)`
   height: 100px;
   font-family: "Lato", sans-serif;
   font-weight: 400;
@@ -41,7 +58,24 @@ const LogoField = styled.div`
 `;
 
 const SignInButton = styled.p`
-    font-size: 14px;
-    color: #2a2a2b;
+  text-align: center;
+  font-size: 14px;
+  color: #2a2a2b;
+  cursor: pointer;
+`;
+
+const NavContainer = styled(Container)`
+  background: #2a2a2b;
+  height: 75px;
+`;
+
+const NavLinksBlock = styled.div`
+  display: flex;
+  justify-content: space-around;
+  
+  p {
+    text-transform: uppercase;
+    color: #fff;
     cursor: pointer;
+  }
 `;
