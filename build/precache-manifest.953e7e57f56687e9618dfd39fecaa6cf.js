b6c3b4d403a238362635fbeed8ce84306599acfd
JuStPiNkCoDeR <44189902+JuStPiNkCoDeR@users.noreply.github.com>
2020-05-13 22:47:27 +0300

    Build version 1.00A

---
+++ build/precache-manifest.953e7e57f56687e9618dfd39fecaa6cf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8410dbb1148f853eb3886db98fa5540",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c81c8fdf741fc74a706",
+    "url": "/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "954a91496deb7f45a2f0",
+    "url": "/static/css/main.5d12bb6d.chunk.css"
+  },
+  {
+    "revision": "0c81c8fdf741fc74a706",
+    "url": "/static/js/2.7620d5b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7620d5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954a91496deb7f45a2f0",
+    "url": "/static/js/main.386f3848.chunk.js"
+  },
+  {
+    "revision": "31e0701abc1492569459",
+    "url": "/static/js/runtime-main.abd89b3e.js"
+  },
+  {
+    "revision": "cd7f291e80285ef087f98c00249d9ed4",
+    "url": "/static/media/chipBg.cd7f291e.jpg"
+  },
+  {
+    "revision": "8b89be1b311671c7a19d95ac6607cb3d",
+    "url": "/static/media/item.8b89be1b.png"
+  },
+  {
+    "revision": "a4cfaef2760f3119fb3485b0c8cbde62",
+    "url": "/static/media/technicalInfoBg.a4cfaef2.png"
+  }
+]);
