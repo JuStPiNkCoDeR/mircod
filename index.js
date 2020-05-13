65edd1be7ece4d44d9c8a2aa3f5b32c946dc31b1
JuStPiNkCoDeR <44189902+JuStPiNkCoDeR@users.noreply.github.com>
2020-05-13 23:02:42 +0300

    Adapt for Heroku deploy

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname + './build/static')));
+app.use('/styles', express.static(__dirname + './build/static/css'));
+app.use('/images', express.static(__dirname + './build/static/media'));
+app.use('/scripts', express.static(__dirname + './build/static/js'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + './build/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
